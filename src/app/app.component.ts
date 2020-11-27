52fcce53d86c99f75698e11095cf5178f75a720c
EXT02D18761 <darya.lazouskaya@consultant.turkcell.com.tr>
2020-11-27 11:36:04 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'jok';
+  update: boolean;
+  public joke: any;
+
+  constructor(updates: SwUpdate, private dataService: DataService) {
+    updates.available.subscribe(ev => {
+      updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
+  ngOnInit() {
+    this.dataService.giveJokes().subscribe(res => {
+      this.joke = res;
+    })
+  }
 }
